4eee74a89aa278b61f7ca606c6aca670aa386fa4
Scherbakov Anatoly <axidarr@gmail.com>
2022-10-22 20:47:09 +0300

    Added Homework4

--- Seminar4/Homework4.py
+++ Seminar4/Homework4.py
@@ -1,4 +1,5 @@
 import random
+from decimal import Decimal
 
 def Enter_number(text, type = 1): # функция ввода числа с проверкой 
     print(text)
@@ -15,25 +16,25 @@ def Enter_number(text, type = 1): # функция ввода числа с пр
                     value = float(input())
                     break
                 elif type == 3:
-                    from decimal import Decimal
                     value = Decimal(input())
                     break
             except:
                 print("Ошибка! Введите число:")
     return value
 
+
 # 1. Вычислить число c заданной точностью d
-# Пример: - при d = 0.001, π = 3.141.    10^(-1) ≤ d ≤10^(-10)
+# Пример: - при d = 0.001, π = 3.141, 10^(-1) ≤ d ≤10^(-10)
 # е = 1 + 1/1! + 1/2! + 1/3! + ... = 2,7182818284590452353602874713527…
 
 def number_e(acc):
+    acc = abs(acc)
     d = 1 / (10 ** acc)
-    e = 1
-    fact_e = 1
+    e = Decimal(1)
+    fact_e = Decimal(1)
 
     count = 1
     while True:
-        print(e)
         fact_e *= count
         temp_e = e + 1 / (fact_e)
         if (abs(e - temp_e) <= d):
@@ -42,8 +43,10 @@ def number_e(acc):
         e = temp_e    
         count += 1
 
-""" accuracy = abs(Enter_number('Введите требуемую точность числа "e" после запятой: '))
-print(f'e = {number_e(accuracy)}\n') """
+print(f'\n____1. Вычисление числа "e"c заданной точностью____\n')
+accuracy = Enter_number('Введите требуемую точность числа "e" после запятой: ')
+print(f'e = {number_e(accuracy)}')
+
 
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
@@ -63,12 +66,15 @@ def prime_factors(num):
             else: print(factors_list[i])
     else: print(f'Число {num} - простое')
 
-""" while True:
+print(f'\n____2. Вывод списка простых множителей введённого числа____\n')
+
+while True:
     number = Enter_number('Введите натуральное число: ')
     if number > 0: break
     else: print('Натуральное число должно быть больше нуля!')
 
-prime_factors(number) """
+prime_factors(number)
+
 
 # 3. Задайте последовательность чисел. Напишите программу, которая выведет список
 # неповторяющихся элементов исходной последовательности.
@@ -86,65 +92,65 @@ def unique_elements(main_list):
     
     return unique_list
 
-""" num_list = [random.randint(1, 9) for i in range(random.randint(1, 29))]
+print(f'\n____3. Вывод списка неповторяющихся элементов исходной последовательности____\n')
+
+num_list = [random.randint(1, 9) for i in range(random.randint(1, 29))]
 print(f'Заданная последовательность чисел:\n{num_list}')
-print(f'Список неповторяющихся элементов исходной последовательности:\n{unique_elements(num_list)}') """
+print(f'Список неповторяющихся элементов исходной последовательности:\n{unique_elements(num_list)}')
+
 
 # 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
 # (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 # Пример: - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-def polynomial_generate(number):
-    pol_list = []
-    while number != 0:
+def poly_gen(pow): # генерация многочлена по заданной степени
+    poly_list = []
+    while pow != 0:
         k = str(random.randint(0, 100))
-        if number > 1:
-            if k != '0' and k != '1': pol_list.append(f'{k}x^{str(number)}')
-            elif k == '1': pol_list.append(f'x^{str(number)}')
-        elif number == 1:
-            if k != '0' and k != '1': pol_list.append(f'{k}x')
-            elif k == '1': pol_list.append(f'x')
-        number -= 1
-    pol_list.append(str(random.randint(1, 100)))
-    return pol_list
+        if pow > 1:
+            if k != '0' and k != '1': poly_list.append(f'{k}x^{str(pow)}')
+            elif k == '1': poly_list.append(f'x^{str(pow)}')
+        elif pow == 1:
+            if k != '0' and k != '1': poly_list.append(f'{k}x')
+            elif k == '1': poly_list.append(f'x')
+        pow -= 1
+    poly_list.append(str(random.randint(0, 100)))
+    return poly_list
 
-def list_to_file(lst, n = 1):
+def list_to_file(lst, n = 1): # запись многочлена из списка в файл и вывод его на экран
     file = str(f'file{n}.txt')
-    with open(file, 'w') as data:
+    with open(file, 'w', encoding = "utf-8") as data:
         for i in range(len(lst) - 1):
             data.write(f'{lst[i]} + ')
         data.write(lst[-1])
-    print(f'Создан файл file{n}.txt с этим многочленом')
+    print(f'Создан файл file{n}.txt с многочленом:')
+    with open(file, 'r', encoding = "utf-8") as data:
+        print(f'{data.read()}\n')
 
-""" while True:
+print(f'\n____4. Запись в файл многочлена заданной степени со случайными коэффициентами____\n')
+
+while True:
     power = Enter_number('Введите натуральную степень многочлена: ')
     if power > 0: break
     else: print('Натуральное число должно быть больше нуля!')
 
-polynomial1 = polynomial_generate(power)
-print(polynomial1)
-list_to_file(polynomial1)
+poly1 = poly_gen(power)
+list_to_file(poly1)
 
-polynomial2 = polynomial_generate(power)
-print(polynomial2)
-list_to_file(polynomial2, 2) """
+poly2 = poly_gen(power)
+list_to_file(poly2, 2)
+
 
 # 5. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
 
-# text = text.encode('utf8')
-
-# with open('file.txt', 'w', encoding = "utf-8") as data:
-    # data.write('текст')
-
-
-def list_from_file(file):
-    with open(file, 'r') as data:
+def list_from_file(file): # возврат многочлена из файла в виде списка 
+    with open(file, 'r', encoding = "utf-8") as data:
         str = data.read()
     pol_list = str.split(' + ')
     return pol_list
 
-def strlist_to_intlist(lst):
+def strlist_to_intlist(lst): # преобразование многочлена из строкового списка членов в интовый список списков (коэффициент, степень члена)
     list_of_lst = []
 
     for i in range(len(lst)):
@@ -162,28 +168,60 @@ def strlist_to_intlist(lst):
 
     return list_of_lst
 
-def polynomials_sum(lst1, lst2):
+def polynomials_sum(lst1, lst2): # возврат суммы двух интовых многочленов в виде интового списка списков
     lst_sum = []
+    check = False
 
     if len(lst1) >= len(lst2):
         for i in range(len(lst1)):
-            for j in range(len(lst1[i])):
-                for k in range(len(lst2)):
-                    for l in range(len(lst2[k])):
-                        if lst1[i][j] == lst1[k][l]:
-                            lst_sum.append()
-
+            for j in range(len(lst2)):
+                if lst1[i][1] == lst2[j][1]:
+                    lst_sum.append([lst1[i][0] + lst2[j][0], lst1[i][1]])
+                    check = True
+            if check == False: lst_sum.append([lst1[i][0], lst1[i][1]])
+            check = False
+    else:
+        for i in range(len(lst2)):
+            for j in range(len(lst1)):
+                if lst2[i][1] == lst1[j][1]:
+                    lst_sum.append([lst2[i][0] + lst1[j][0], lst2[i][1]])
+                    check = True
+            if check == False: lst_sum.append([lst2[i][0], lst2[i][1]])
+            check = False
+    
     return lst_sum
 
-poly1 = list_from_file('file1.txt')
-#print(poly1)
-print(f'{strlist_to_intlist(poly1)}\n')
+def intlist_to_str(lst_of_lst): # преобразование интового списка списков многочлена в строку
+    poly_str = ''
 
-poly2 = list_from_file('file2.txt')
-#print(poly2)
-print(f'{strlist_to_intlist(poly2)}\n')
+    for i in range(len(lst_of_lst)):
+        if lst_of_lst[i][1] > 1:
+            if lst_of_lst[i][0] != 1: poly_str += (f'{str(lst_of_lst[i][0])}x^{str(lst_of_lst[i][1])} + ')
+            else: poly_str += (f'x^{str(lst_of_lst[i][1])} + ')
+        elif lst_of_lst[i][1] == 1:
+            if lst_of_lst[i][0] != 1: poly_str += (f'{str(lst_of_lst[i][0])}x + ')
+            else: poly_str += (f'x + ')
+        else:
+            poly_str += (f'{str(lst_of_lst[i][0])}')        
+        
+    return poly_str
 
-# poly2 = list_from_file('file2.txt')
-# print(poly2)
-# poly_sum = list(zip(poly1, poly2))
-# print(poly_sum)
+def str_to_file(str_poly, n = 1): # запись строки в файл и вывод результата на экран
+    file = str(f'file{n}.txt')
+    with open(file, 'w', encoding = "utf-8") as data:
+        data.write(str_poly)
+    print(f'Создан файл file{n}.txt с этим многочленом\n')
+
+
+print(f'____5. Запись в файл многочлена из суммы двух многочленов, записанных в отдельных файлах____\n')
+
+poly1_ext = list_from_file('file1.txt')
+poly1_ext_int = strlist_to_intlist(poly1)
+
+poly2_ext = list_from_file('file2.txt')
+poly2_ext_int = strlist_to_intlist(poly2)
+
+print(f'Сумма многочленов из предыдущей задачи:\n')
+sum_pol = intlist_to_str(polynomials_sum(poly1_ext_int, poly2_ext_int))
+print(sum_pol)
+str_to_file(sum_pol, 3)
